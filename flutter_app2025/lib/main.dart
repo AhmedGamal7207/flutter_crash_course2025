25f54beb5a40646ff1c89a12f86b4b833690d79c
AhmedGamal7207 <92176653+AhmedGamal7207@users.noreply.github.com>
2025-02-25 01:12:29 +0200

    Stateful Widget First Trial
    Testing the stateful widget to control a bottom navigation bar

--- flutter_app2025/lib/main.dart
+++ flutter_app2025/lib/main.dart
@@ -4,6 +4,8 @@ void main() {
   runApp(const MyApp());
 }
 
+String? title = "Jimmy";
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -11,62 +13,46 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Jimmy",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.teal,
           brightness: Brightness.dark,
         ),
       ),
-      home: Scaffold(
-        appBar: AppBar(title: Text("Jimmy App"), centerTitle: true),
-        bottomNavigationBar: NavigationBar(
-          destinations: [
-            NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-            NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
-          ],
-          onDestinationSelected: (value) {
-            print(value);
-          },
-          selectedIndex: 0,
-        ),
-        floatingActionButton: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            FloatingActionButton(
-              onPressed: () {
-                print("Clicked");
-              },
-              child: Icon(Icons.add),
-            ),
-            SizedBox(height: 10),
-            FloatingActionButton(
-              onPressed: () {
-                print("Clicked");
-              },
-              child: Icon(Icons.add),
-            ),
-          ],
-        ),
-        drawer: Drawer(
-          child: DrawerHeader(
-            child: Column(
-              children: [
-                ListTile(
-                  title: Text("About Us"),
-                  leading: Icon(Icons.info_rounded),
-                  onTap: () {},
-                ),
-                ListTile(
-                  title: Text("Logout"),
-                  leading: Icon(Icons.logout),
-                  onTap: () {},
-                ),
-              ],
-            ),
-          ),
-        ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int navigationBarIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title!), centerTitle: true),
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+          NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
+        ],
+        onDestinationSelected: (value) {
+          setState(() {
+            navigationBarIndex = value;
+          });
+        },
+        selectedIndex: navigationBarIndex,
       ),
+      body:
+          navigationBarIndex == 0
+              ? Center(child: Text("Home"))
+              : Center(child: Text("Profile")),
     );
   }
 }
